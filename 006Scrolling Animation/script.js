6060ed029cd2ad02b9f7536a4dfb06892a54dd59
8e9one <33422357+8e9one@users.noreply.github.com>
2022-04-27 12:49:59 +0200

    Add files via upload

---
+++ 006Scrolling Animation/script.js
@@ -0,0 +1,21 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    //https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
